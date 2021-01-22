e4c5025211dcd8fcb2e13ff2c4dc56274c9ac447
Mohammed AbuMeizer <mohammed.abumeizer@gmail.com>
2021-01-22 02:46:29 +0200

    complete the exercises

--- Week_3/Day_4/Daily Challenge Not Bad/myscript.js
+++ Week_3/Day_4/Daily Challenge Not Bad/myscript.js
@@ -1,9 +1,8 @@
-let sentence = "The movie is not that bad really, I like it" ;
+let sentence = "The movie is no that bad really, I like it" ;
 let wordNot =0 ;
 let wordBad =0;
 let replceWord = "";
 
-let sln=sentence.length;
 
 wordNot = sentence.indexOf("not");//first index of not
 wordBad = sentence.indexOf("bad");//first index of bad
@@ -12,12 +11,15 @@ wordBad = sentence.indexOf("bad");//first index of bad
 
 //-1 if there is no word using or 
 
-if (wordNot < wordBad || wordNot == -1) {
+if (wordNot < wordBad && wordNot != -1) {
 	replceWord = sentence.slice(wordNot,wordBad+3);
 	sentence = sentence.replace(replceWord,"good");
 	console.log(sentence);
 	document.getElementById("result").innerHTML = sentence.toString() ;
-}else if (wordBad == -1 || wordBad == -1) {
-		document.getElementById("result").innerHTML = sentence.toString() ;
+}else if (wordBad == -1 || wordNot == -1) {
+	document.getElementById("result").innerHTML = sentence.toString() ;
+
+}else{
+	document.getElementById("result").innerHTML = sentence.toString() ;
 
 }
